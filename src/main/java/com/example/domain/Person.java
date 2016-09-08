a048176b2e34ac422a63e7d68848a6bd659b201d
SanderEker <ekersander@gmail.com>
2016-09-08 11:35:05 +0200

    @override - to string aangepast

--- src/main/java/com/example/domain/Person.java
+++ src/main/java/com/example/domain/Person.java
@@ -17,6 +17,7 @@ public class Person {
 	public long getId() {
 		return id;
 	}
+	
 	public void setId(long id) {
 		this.id = id;
 	}
@@ -24,21 +25,27 @@ public class Person {
 	public String getFirstname() {
 		return firstname;
 	}
+	
 	public void setFirstname(String firstname) {
 		this.firstname = firstname;
 	}
+	
 	public String getLastname() {
 		return lastname;
 	}
+	
 	public void setLastname(String lastname) {
 		this.lastname = lastname;
 	}
+	
 	public String getAge() {
 		return age;
 	}
+	
 	public void setAge(String age) {
 		this.age = age;
 	}
+	
 	@Override
 	public String toString() {
 		return "Person [id=" + id + ", firstname=" + firstname + ", lastname=" + lastname + ", age=" + age + "]";
